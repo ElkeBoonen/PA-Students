c186281eca68073d63fdb65f097bda7e0c381e5a
Elke Boonen <u0116591@thomasmore.be>
2021-10-07 12:24:44 +0200

    les 7/10

---
+++ 03 Sort it out/Sort - IMS/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TM.ProgrammingAdvanced;
+
+namespace Sort___IMS
+{
+    class Program
+    {
+
+        const int b1 = 1;
+        const int b2 = 50;
+
+        static int Map(int s, int a1, int a2)
+        {
+            return (int)(b1 + (((s - a1) * (b2 - b1)) / (a2 - a1)));
+        }
+        static void Print(int[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void Print(KeyValuePair<string,int>[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> songs = Data.Songs;
+            int a1 = songs.Values.ToArray().Min();
+            int a2 = songs.Values.ToArray().Max();
+
+            /*foreach (var item in songs)
+            {
+                Console.WriteLine(item.Key + " " + Map(item.Value, a1, a2));
+            }*/
+
+            int[] array = new int[] { 24, 32, 58, 1, 5, 9, 16 };
+            Print(array);
+            SortMethods methods = new SortMethods();
+            methods.Bubble(array);
+            Print(array);
+
+            KeyValuePair<string, int>[] arraySongs = songs.ToArray();
+            methods.Bubble(arraySongs);
+            Print(arraySongs);
+
+
+        }
+    }
+}
---
+++ 03 Sort it out/Sort - IMS/SortMethods.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort___IMS
+{
+    class SortMethods
+    {
+        public void Print(int[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+        public void Bubble(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length-i-1; j++)
+                {
+                    int swap;
+                    if (array[j] > array[j + 1])
+                    {
+                        swap = array[j];
+                        array[j] = array[j+1];
+                        array[j + 1] = swap; 
+                    }
+                    //Print(array);
+                }
+            }
+        }
+
+        public void Bubble(KeyValuePair<string, int>[] songs)
+        {
+
+            for (int i = 0; i < songs.Length; i++)
+            {
+                for (int j = 0; j < songs.Length - i - 1; j++)
+                {
+                    KeyValuePair<string, int> swap;
+
+                    if (songs[j].Value > songs[j+1].Value)
+                    {
+                        swap = songs[j];
+                        songs[j] = songs[j + 1];
+                        songs[j + 1] = swap;
+                        
+                    }
+                }
+            }
+        }
+    }
+}
