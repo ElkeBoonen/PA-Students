306f8ad640bfa28ee1326c4ca79f8ab749b978ff
Elke Boonen <u0116591@thomasmore.be>
2021-11-18 19:02:36 +0100

    les 18/11

---
+++ 06 DC/DC - DSPS/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DC___DSPS
+{
+    class Program
+    {
+        static void Print(List<int> list)
+        {
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void Print(int[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+
+        static void Main(string[] args)
+        {
+            DC dc = new DC();
+
+            Console.WriteLine(dc.Squares(168,64));
+            Console.WriteLine(dc.Squares(80, 20));
+
+            Console.WriteLine(dc.SquaresIterative(168, 64));
+            Console.WriteLine(dc.SquaresIterative(80, 20));
+
+            int[] array = { -1, 5, 8, 7, 10, -5, 6, 3, 4, 1, 2 };
+            Console.WriteLine(dc.SumIterative(array));
+            Console.WriteLine(dc.Sum(0,array));
+            Console.WriteLine(dc.Sum(array.ToList()));
+            Console.WriteLine(dc.SumOne(array.ToList()));
+
+            Print(array);
+            Print(dc.QuickSort(array.ToList()));
+
+            dc.Hanoi(10, 'A', 'C', 'B');
+        }
+    }
+}
---
+++ 06 DC/DC - IMS/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DC___IMS
+{
+    class Program
+    {
+        static void Print(List<int> list)
+        {
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void Print(int[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            DC dc = new DC();
+            Console.WriteLine(dc.Squares(168, 64));
+            Console.WriteLine(dc.SquaresSlides(168, 64));
+            Console.WriteLine(dc.SquaresIterative(168, 64));
+
+            int[] array = { 2, 6, 4 };
+            Console.WriteLine(dc.SumIterative(array));
+            Console.WriteLine(dc.Sum(0, array));
+            Console.WriteLine(dc.SumAron(array, 0));
+            Console.WriteLine(dc.SumSlides(array.ToList()));
+
+            array = new int[] { 10, -5, 7, 9, -1, 5, 4, 3, 15, 2, -2};
+            Print(array);
+            List<int> sortedlist = dc.QuickSort(array.ToList());
+            Print(sortedlist);
+
+            dc.Hanoi(5, 'A', 'C', 'B');
+
+
+        }
+    }
+}
