82709f99d966168d66bdd297f9619ba954828761
Elke Boonen <u0116591@thomasmore.be>
2021-11-25 11:51:42 +0100

    les 25/11

---
+++ 08 Graph/Graphs/Maze.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    class Maze
+    {
+        private List<int>[] _maze;
+
+        public Maze(int nodes)
+        {
+            _maze = new List<int>[nodes];
+        }
+
+        //addEdge(0,1)
+        public void AddEdge(int nodeA, int nodeB)
+        {
+            if (_maze[nodeA]==null)
+            {
+                _maze[nodeA] = new List<int>();
+            }
+            _maze[nodeA].Add(nodeB);
+
+            if (_maze[nodeB] == null)
+            {
+                _maze[nodeB] = new List<int>();
+            }
+            _maze[nodeB].Add(nodeA);
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            for (int i = 0; i < _maze.Length; i++)
+            {
+                s += i + " --> ";
+                if (_maze[i] != null)
+                {
+                    foreach (var item in _maze[i])
+                    {
+                        s += item + " ";
+                    }
+                }
+                s += Environment.NewLine;
+            }
+            return s;
+        }
+
+    }
+}
