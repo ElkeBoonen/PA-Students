cd339eeb2bcb21f0e04c0506b22ccddcff50f881
Elke Boonen <u0116591@thomasmore.be>
2021-12-16 12:38:55 +0100

    les 16/1

--- 11 Knapsack/Knapsack - IMS/Knapsack.cs
+++ 11 Knapsack/Knapsack - IMS/Knapsack.cs
@@ -16,5 +16,54 @@ namespace Knapsack___IMS
             Items = items;
             MaxWeight = weight;
         }
+
+        //weight 1, value 10
+        //weight 2, value 5
+        //weight 8, value 40
+        //weight 10, value 45
+        public int Greedy()
+        {
+            Items.Sort(); //sorteren op value: 5 10 40 45
+            Items.Reverse(); // --> 45 40 10 5
+
+            int weight = 0;
+            int value = 0;
+
+            for (int i = 0; i < Items.Count; i++)
+            {
+                if (weight + Items[i].Weight <= MaxWeight)
+                {
+                    weight += Items[i].Weight;
+                    value += Items[i].Value;
+                }
+            }
+            return value;
+        }
+
+        public int BruteForce()
+        {
+            List<int> values = new List<int>();
+            for (int i = 0; i < (int)Math.Pow(2,Items.Count); i++)
+            {
+                string binair = Convert.ToString(i, 2).PadLeft(Items.Count,'0');
+
+                int weight = 0;
+                int value = 0;
+                for (int j = 0; j < binair.Length; j++)
+                {
+                    if (binair[j]=='1')
+                    {
+                        weight += Items[j].Weight;
+                        value += Items[j].Value;
+                    }
+                    if (weight <= this.MaxWeight)
+                    {
+                        values.Add(value);
+                    }
+                }
+            }
+            values.Sort();
+            return values[values.Count-1];
+        }
     }
 }
--- 11 Knapsack/Knapsack - IMS/Program.cs
+++ 11 Knapsack/Knapsack - IMS/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Knapsack___IMS
 {
@@ -6,7 +7,16 @@ namespace Knapsack___IMS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Item> items = new List<Item>();
+            items.Add(new Item(1, 10)); //weight 1, value 10
+            items.Add(new Item(2, 5));  //weight 2, value 5
+            items.Add(new Item(8, 40)); //weight 8, value 40
+            items.Add(new Item(10, 45));//weight 10, value 45
+
+            Knapsack knapsack = new Knapsack(items, 10);
+            Console.WriteLine("GREEDY: " + knapsack.Greedy());
+            Console.WriteLine("BRUTE FORCE: " + knapsack.BruteForce());
+
         }
     }
 }
