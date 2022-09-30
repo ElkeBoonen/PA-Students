498d7bf29f4b440c904905685e1fdb9f50ae0c48
Elke Boonen <u0116591@thomasmore.be>
2022-09-30 14:55:48 +0200

    DSPS 30/9

---
+++ 02 Datastructures/Datastructures - DSPS/LinkedList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Datastructures___DSPS
+{
+    class LinkedList
+    {
+        public Node Head { get; private set; }
+
+        public LinkedList()
+        {
+            Head = null;
+        }
+
+        public void InsertFront(string data)
+        {
+            Node newnode = new Node(data);
+            newnode.Next = Head;
+            Head = newnode;
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            Node temp = Head;
+            while (temp != null)
+            {
+                s += temp.Data + " -> ";
+                temp = temp.Next;
+            }
+            return s.Substring(0,s.Length-4);
+        }
+    }
+}
---
+++ 02 Datastructures/Datastructures - DSPS/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Datastructures___DSPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.InsertFront("apple");
+            list.InsertFront("banana");
+            list.InsertFront("lemon");
+
+            Console.WriteLine(list);
+        }
+    }
+}
