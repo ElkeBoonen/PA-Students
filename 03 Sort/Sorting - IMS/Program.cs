75a6a0defb71720ba260cd62107fdcc879146456
Elke Boonen <u0116591@thomasmore.be>
2022-10-06 15:12:56 +0200

    IMS 6/10

---
+++ 03 Sort/Sorting - IMS/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TM.ProgrammingAdvanced;
+
+namespace Sorting___IMS
+{
+    class Program
+    {
+        static int[] MapValues(int[] array, int b1, int b2)
+        {
+            int a1 = array.Min(); int a2 = array.Max();
+            int[] newValues = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                newValues[i] = b1 + ((array[i] - a1) * (b2 - b1) / (a2 - a1));
+            }
+            return newValues;
+        }
+
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> songs = Data.Songs;
+            /*foreach (KeyValuePair<string, int> pair in songs)
+            { 
+                Console.WriteLine(pair.Value);
+            }*/
+
+            int[] array = MapValues(songs.Values.ToArray(), 0, 100);
+            //Console.WriteLine(String.Join(" ", array));
+
+            Sorting sorting = new Sorting();
+            int[] int_array = new int[] { 8, 9, 4, 1, 7 };
+            sorting.Bubble(int_array);
+            Console.WriteLine(String.Join(" ", int_array));
+
+            KeyValuePair<string, int>[] songs_array = songs.ToArray();
+            sorting.Bubble(songs_array);
+            Console.WriteLine(String.Join("\n", songs_array));
+        }
+    }
+}
---
+++ 03 Sort/Sorting - IMS/Sorting.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorting___IMS
+{
+    public class Sorting
+    {
+        public void Bubble(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length-1-i; j++)
+                {
+                    if (array[j] > array[j+1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j+1];
+                        array[j+1] = temp;
+                    }
+                }
+            }
+        }
+
+        public void Bubble(KeyValuePair<string,int>[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1 - i; j++)
+                {
+                    if (array[j].Value > array[j + 1].Value)
+                    {
+                        KeyValuePair<string, int> temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+    }
+}
