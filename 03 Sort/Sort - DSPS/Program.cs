de31889bb30ebf19c6df36eb989afdb28d5ad795
Elke Boonen <u0116591@thomasmore.be>
2022-10-07 14:26:23 +0200

    DSPS

---
+++ 03 Sort/Sort - DSPS/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Sort___DSPS
+{
+    internal class Program
+    {
+        static public Dictionary<string, int> ReadData(string file)
+        { 
+            Dictionary<string, int> data = new Dictionary<string, int>();
+            foreach (var item in File.ReadAllLines(file))
+            {
+                //rank,uri,artist_names,track_name,source,peak_rank,previous_rank,weeks_on_chart,streams
+                string[] items = item.Replace(", ", " ").Split(",");
+                string title = items[3].Replace("\"", "");
+                int streamcount;
+
+                if (Int32.TryParse(items[8].Replace("\"", ""), out streamcount))
+                {
+                    data[title] = streamcount;
+                }
+                
+            }
+            return data;
+        }
+
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> data = ReadData("data.csv");
+            foreach (var item in data)
+            {
+                Console.WriteLine(item.Key + " --> " + item.Value);
+            }
+        }
+    }
+}
