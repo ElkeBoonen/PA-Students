225efb39d90b3f5c7679c587e718763340c4c4a5
Elke Boonen <u0116591@thomasmore.be>
2022-10-20 15:20:36 +0200

    IMS 20/10

---
+++ 05 DC/DC - IMS/DC.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace DC___IMS
+{
+    class DC
+    {
+        public int SumI(int[] array)
+        {
+            int sum = 0;
+            foreach (int i in array)
+            {
+                sum += i;
+            }
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    sum += array[i];
+            //}
+
+            return sum;
+        }
+        public int SumRL(List<int> list)
+        {
+            if (list.Count == 0) return 0;
+            int number = list[0];
+            list.RemoveAt(0);
+            return number + SumRL(list);
+        }
+
+        public int SumRA(int[] array, int index = 0)
+        {
+            if (index == array.Length) return 0;
+
+            return array[index] + SumRA(array, ++index);
+        }
+
+        public List<int> QuickSort(List<int> list)
+        { 
+            
+        }
+    }
+}
---
+++ 05 DC/DC - IMS/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DC___IMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 11, 2, -3, 4, -6, 10, 52, 36, 24 };
+
+            DC dc = new DC();
+            Console.WriteLine(dc.SumI(array));
+            Console.WriteLine(dc.SumRL(array.ToList()));
+            Console.WriteLine(dc.SumRA(array));
+
+            List<int> list = dc.QuickSort(array.ToList());
+            Console.WriteLine(String.Join(" ", list));
+
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+
+        }
+    }
+}
