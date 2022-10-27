7306aed47df63c147fa6c75977d659a932b0da47
Elke Boonen <u0116591@thomasmore.be>
2022-10-27 15:18:33 +0200

    IMS 27/10

---
+++ 06 DP/Fibonaccu - IMS/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fibonacci___IMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci fib = new Fibonacci();
+            Console.WriteLine("\nITERATIEF: " + fib.Iterative(20));
+            Console.WriteLine("\nRECURSIEF: " + fib.Recursive(20));
+            Console.WriteLine("\nMEMOIZATION: " + fib.Memoization(20));
+            Console.WriteLine("\nTABULATION: " + fib.Memoization(20));
+
+
+
+            //Console.WriteLine(fib.Iterative(40));
+            //Console.WriteLine(fib.Recursive(40));
+
+
+
+        }
+    }
+}
