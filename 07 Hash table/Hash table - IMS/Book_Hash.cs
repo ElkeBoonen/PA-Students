456bd3d1b0b42caf5dfd9be160bd1c104783aac0
Elke Boonen <u0116591@thomasmore.be>
2022-11-17 15:17:28 +0100

    IMS 17/11

---
+++ 07 Hash table/Hash table - IMS/Book_Hash.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hash_table
+{
+    internal class Book_Hash
+    {
+    }
+}
---
+++ 07 Hash table/Hash table - IMS/Book_KVP.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hash_table
+{
+    class Book_KVP
+    {
+        private KeyValuePair<string, double>[] book;
+        private int index = 0;
+
+        public Book_KVP(int items)
+        {
+            book = new KeyValuePair<string, double>[items];
+        }
+
+        public void AddItem(string key, double value)
+        {
+            KeyValuePair<string, double> item = new KeyValuePair<string, double>(key, value);
+            book[index] = item;
+            index++;
+        }
+
+        public double GetPrice(string find)
+        {
+            foreach (var item in book)
+            {
+                if (find == item.Key) return item.Value;
+            }
+            return -1;
+        }
+
+        public override string ToString()
+        {
+            string s = string.Empty;
+            for (int i = 0; i < index; i++)
+            {
+                s += book[i].Key + ": " + book[i].Value + "\n";
+            }
+            return s;
+        }
+    }
+}
