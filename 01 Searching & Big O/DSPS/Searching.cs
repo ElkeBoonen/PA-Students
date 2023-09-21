8dd842a522a20121a0a4364762a39f9bfca6c0c3
Elke Boonen <u0116591@thomasmore.be>
2023-09-21 12:36:55 +0200

    DSPS 21/9

---
+++ 01 Searching & Big O/DSPS/Searching.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSPS
+{
+    internal class Searching
+    {
+        public int count { get; set; }
+        public int Linear(int[] array, int number)
+        {
+            count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                count++;
+                if (array[i] == number)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public int Stupid(int[] array, int number)
+        {
+            count = 0;
+            int max_count = array.Length * 2;
+
+            Random random = new Random();
+
+            while (count < max_count)
+            {
+                count++;
+
+                int index = random.Next(0,array.Length);
+                if (array[index] == number)
+                {
+                    return index;
+                }
+            }
+            return -1;
+        }
+
+        public int Binary(int[] array, int number)
+        { 
+            int start = 0;
+            int end = array.Length-1;
+            count = 0;
+
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+                count++;
+
+                if (array[mid] == number)
+                {
+                    return mid;
+                }
+                else if (number < array[mid])
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
