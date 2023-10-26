3086eb0c8dc867879e3de51a1dfc349da29b71f4
Elke Boonen <u0116591@thomasmore.be>
2023-10-26 11:18:15 +0200

    26/10

---
+++ 06 Divide & Conquer/DSPS/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DSPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum sum = new Sum();
+            Console.WriteLine(sum.Iterative(new int[] { 1, 3, 7, 10, 6 }));
+            Console.WriteLine(sum.DCList(new List<int>(new int[] { 1, 3, 7, 10, 6 })));
+            Console.WriteLine(sum.DCList(new List<int>()));
+            Console.WriteLine(sum.DCArray(new int[] { 1, 3, 7, 10, 6 }));
+
+        }
+    }
+}
---
+++ 06 Divide & Conquer/DSPS/Sum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSPS
+{
+    class Sum
+    {
+        public int Iterative(int[] array)
+        {
+            int sum = 0;
+            foreach (var item in array)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+
+        public int DCList(List<int> list)
+        {
+            if (list.Count == 0) return 0;
+            //if (list.Count == 1) return list[0];
+
+            int value = list[0];
+            list.RemoveAt(0);
+            return value + DCList(list);
+        }
+
+        public int DCArray(int[] array, int index=0)
+        {
+            if (index == array.Length) return 0;
+
+            return array[index] + DCArray(array, ++index);
+            //return array[index] + DCArray(array, index + 1);  //OK
+            //return array[index] + DCArray(array, index++);    //PROBLEM!! POST INCREMENTATION
+
+        }
+    }
+}
