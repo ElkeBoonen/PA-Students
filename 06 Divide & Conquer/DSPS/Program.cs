3040c4f0ae67373f8ac8e429dcb9fcf37c0eba00
Elke Boonen <u0116591@thomasmore.be>
2023-10-26 11:57:46 +0200

    26/10

--- 06 Divide & Conquer/DSPS/Program.cs
+++ 06 Divide & Conquer/DSPS/Program.cs
@@ -9,7 +9,17 @@
             Console.WriteLine(sum.DCList(new List<int>(new int[] { 1, 3, 7, 10, 6 })));
             Console.WriteLine(sum.DCList(new List<int>()));
             Console.WriteLine(sum.DCArray(new int[] { 1, 3, 7, 10, 6 }));
+            
 
+            List<int> list = new List<int>(new int[] { 2,7,9,1,10,8,14,21,4,5,18,19 });
+            Console.WriteLine(String.Join(" ", list));
+
+            Quicksort quicksort = new Quicksort();
+            Console.WriteLine(String.Join(" ", quicksort.Sort(list)));
+
+            int[] array = new int[] { 2, 7, 9, 1, 10, 8, 14, 21, 4, 5, 18, 19 };
+            quicksort.Sort(array, 0, array.Length - 1);
+            Console.WriteLine(String.Join(" ", array));
         }
     }
 }
--- 06 Divide & Conquer/DSPS/Quicksort.cs
+++ 06 Divide & Conquer/DSPS/Quicksort.cs
@@ -8,9 +8,69 @@ namespace DSPS
 {
     class Quicksort
     {
+
+        /*Pick an element from the array = the pivot
+          Find elements smaller and greater than the pivot = partitioning!
+          - A sub-array of all the numbers less than the pivot
+          - the pivot
+          - A sub-array of all the numbers greater than the pivot
+          Sort the subarrays with quicksort and merge everything together!*/
         public List<int> Sort(List<int> list)
-        { 
-        
+        {
+            //base case
+            if (list.Count <= 1) return list;
+
+            int pivot = list[0];
+
+            List<int> smaller = new List<int>();
+            List<int> greater = new List<int>();
+
+            foreach (var item in list)
+            {
+                if (item < pivot) smaller.Add(item);
+                else if (item > pivot) greater.Add(item);
+            }
+
+            return Sort(smaller).Union(new List<int> { pivot }).Union(Sort(greater)).ToList<int>();
+
+        }
+
+        public void Sort(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int p = Partition(array, low, high);
+                Sort(array, low, p);
+                Sort(array, p + 1, high);
+            }
+        }
+
+        public int Partition(int[] array, int low, int high)
+        {
+            int pivot = array[(high + low) / 2];
+
+            int i = low - 1;
+            int j = high + 1;
+
+            while (true)
+            {
+                do
+                {
+                    i++;
+                }
+                while (array[i] < pivot);
+
+                do
+                {
+                    j--;
+                }
+                while (array[j] > pivot);
+
+                if (i > j) return j;
+                int swap = array[i];
+                array[i] = array[j];
+                array[j] = swap;
+            }
         }
     }
 }
