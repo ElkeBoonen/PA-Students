98fc11de5bd72959760a6f71d81b3bc7d8ae7631
Elke Boonen <u0116591@thomasmore.be>
2023-11-30 12:00:26 +0100

    30/11

--- 10 Trees/DSPS/BST.cs
+++ 10 Trees/DSPS/BST.cs
@@ -34,7 +34,99 @@ namespace DSPS
 
         private void Add(Node node, int value)
         {
-            //let's code
+            if (node.Value > value)
+            {
+                if (node.Left == null) node.Left = new Node(value);
+                else Add(node.Left, value);
+            }
+            else
+            {
+                if (node.Right == null) node.Right = new Node(value);
+                else Add(node.Right, value);
+            }
+            
+        }
+
+        public void TraverseInOrder()
+        {
+            if (Root != null) TraverseInOrder(Root);
+        }
+        private void TraverseInOrder(Node node)
+        {
+            if (node != null)
+            {
+                TraverseInOrder(node.Left);
+                Console.Write(node.Value + " ");
+                TraverseInOrder(node.Right);
+            }
+        }
+
+        public void TraversePreOrder()
+        {
+            if (Root != null) TraversePreOrder(Root);
+        }
+        private void TraversePreOrder(Node node)
+        {
+            if (node != null)
+            {
+                Console.Write(node.Value + " ");
+                TraversePreOrder(node.Left);
+                TraversePreOrder(node.Right);
+            }
+        }
+
+        public void TraversePostOrder()
+        {
+            if (Root != null) TraversePostOrder(Root);
+        }
+        private void TraversePostOrder(Node node)
+        {
+            if (node != null)
+            {
+                TraversePostOrder(node.Left);
+                TraversePostOrder(node.Right);
+                Console.Write(node.Value + " ");
+
+            }
+        }
+
+
+        public bool Search(int value)
+        {
+            return Search(Root, value);
+        }
+
+        private bool Search(Node current, int value)
+        {
+            if (current == null) return false;
+            if (current.Value == value) return true;
+
+            else if (current.Value > value)
+            {
+                return Search(current.Left, value);
+            }
+            else
+                return Search(current.Right, value);
+        }
+
+        internal int FindMin()
+        {
+            Node node = Root; 
+            while (node.Left != null)
+            {
+                node = node.Left;
+            }
+            return node.Value;
+        }
+
+        internal int FindMax()
+        {
+            Node node = Root;
+            while (node.Right != null)
+            {
+                node = node.Right;
+            }
+            return node.Value;
         }
     }
 }
--- 10 Trees/DSPS/Program.cs
+++ 10 Trees/DSPS/Program.cs
@@ -14,6 +14,24 @@
             tree.Add(4);
             tree.Add(6);
             tree.Add(8);
+
+
+            Console.WriteLine("In-order traversal:");
+            tree.TraverseInOrder();
+
+            Console.WriteLine("\nPre-order traversal:");
+            tree.TraversePreOrder();
+
+            Console.WriteLine("\nPost-order traversal:");
+            tree.TraversePostOrder();
+
+            Console.WriteLine("\nFound 4? " + tree.Search(4));
+            Console.WriteLine("Found 10? " + tree.Search(10));
+
+            Console.WriteLine("Min: " + tree.FindMin());
+            Console.WriteLine("Max: " + tree.FindMax());
+
+
         }
     }
 }
