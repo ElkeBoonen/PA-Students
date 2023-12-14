732fdf53fc4b924c3576e4fb07102ac07849a887
Elke Boonen <u0116591@thomasmore.be>
2023-12-14 10:46:07 +0100

    14/12

--- 12 Greedy/DSPS/Program.cs
+++ 12 Greedy/DSPS/Program.cs
@@ -10,17 +10,21 @@
                  string[] classes = { "art", "eng", "math", "cs", "music"};
                  double[] start = { 9,9.5,10,10.5,11};
                  double[] end = { 10,10.5,11,11.5,12 };
-             
              */
 
-            activities.AddClass("art", 9, 10);
-            activities.AddClass("eng", 9.5, 10.5);
-            activities.AddClass("math", 10, 11);
             activities.AddClass("cs", 10.5, 11.5);
+            activities.AddClass("eng", 9.5, 10.5);
             activities.AddClass("music", 11, 12);
+            activities.AddClass("art", 9, 10);
+            activities.AddClass("math", 10, 11);
 
+            Console.WriteLine(activities.ToString());
 
-            activities.Schedule();
+            List<Class> schedule = activities.Schedule();
+            for (int i = 0; i < schedule.Count; i++)
+            {
+                Console.WriteLine($"class {i + 1}: {schedule[i].Name}");
+            }
         }
     }
 }
--- 12 Greedy/DSPS/Scheduling.cs
+++ 12 Greedy/DSPS/Scheduling.cs
@@ -1,7 +1,7 @@
 ﻿namespace DSPS
 {
 
-    public class Class : IComparable
+    public class Class : IComparable<Class>
     {
         public string Name { get; set; }
         public double Start { get; set; }
@@ -14,28 +14,49 @@
             End = end;
         }
 
-        public int CompareTo(object? obj)
+        public int CompareTo(Class other)
         {
-            throw new NotImplementedException();
+            if (this.End < other.End) return -1;
+            if (this.End > other.End) return 1;
+            return 0;
         }
     }
     public class Scheduling
     {
         List<Class> classes = new List<Class>();
 
-        public Scheduling()
-        {
-            classes.Sort();
-        }
-
         public void AddClass(string name, double start, double end)
         {
             classes.Add(new Class(name, start, end));
         }
 
-        internal void Schedule()
+        public List<Class> Schedule()
         {
-            throw new NotImplementedException();
+            classes.Sort();
+            List<Class> schedule = new List<Class>();
+
+            schedule.Add(classes[0]);
+
+            for (int i = 1; i < classes.Count; i++)
+            {
+                if (classes[i].Start >= schedule[schedule.Count - 1].End)
+                { 
+                    schedule.Add(classes[i]);
+                }
+            }
+
+            return schedule;
+        }
+
+        public override string ToString()
+        {
+            classes.Sort();
+            string s = "";
+            foreach (var item in classes)
+            {
+                s += item.Name + " " + item.Start + ":" + item.End + "\n";
+            }
+            return s;
         }
     }
 }
