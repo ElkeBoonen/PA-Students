631461c5e8f7c9619a4c38c56ccc3e07dc533add
Elke Boonen <u0116591@thomasmore.be>
2023-12-14 10:20:50 +0100

    14/12

--- 12 Greedy/DSPS/Program.cs
+++ 12 Greedy/DSPS/Program.cs
@@ -6,7 +6,19 @@
         {
             Scheduling activities = new Scheduling();
 
-            activities.
+            /*
+                 string[] classes = { "art", "eng", "math", "cs", "music"};
+                 double[] start = { 9,9.5,10,10.5,11};
+                 double[] end = { 10,10.5,11,11.5,12 };
+             
+             */
+
+            activities.AddClass("art", 9, 10);
+            activities.AddClass("eng", 9.5, 10.5);
+            activities.AddClass("math", 10, 11);
+            activities.AddClass("cs", 10.5, 11.5);
+            activities.AddClass("music", 11, 12);
+
 
             activities.Schedule();
         }
