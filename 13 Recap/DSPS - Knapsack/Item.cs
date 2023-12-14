dc55245bc8886679e4453593e213275beab571ec
Elke Boonen <u0116591@thomasmore.be>
2023-12-14 11:49:25 +0100

    14/12

---
+++ 13 Recap/DSPS - Knapsack/Item.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+
+namespace DSPS___Knapsack
+{
+    public class Item : IComparable<Item>
+    {
+        public int Value { get; set; }
+        public int Weight { get; set; }
+
+        public Item(int value, int weight)
+        {
+            Value = value;
+            Weight = weight;
+        }
+        public int CompareTo(Item other)
+        {
+            if (this.Value < other.Value) return -1;
+            if (this.Value > other.Value) return 1;
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return $"Item V:{this.Value} W:{this.Weight}";
+        }
+    }
+}
---
+++ 13 Recap/DSPS - Knapsack/Knapsack.cs
@@ -0,0 +1,24 @@
+﻿namespace DSPS___Knapsack
+{
+    public class Knapsack
+    {
+        private List<Item> items;
+        private int maxWeight;
+
+        public Knapsack(List<Item> list, int v)
+        {
+            items = list;
+            maxWeight = v;
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            foreach (var item in items)
+            {
+                s += item.ToString() + "\n";
+            }
+            return s;
+        }
+    }
+}
