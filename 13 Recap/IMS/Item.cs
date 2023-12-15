341b168b655cef14c472fc027340defecd9c9510
Elke Boonen <u0116591@thomasmore.be>
2023-12-15 12:20:55 +0100

    15/12

---
+++ 13 Recap/IMS/Item.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace IMS
+{
+    internal class Item : IComparable<Item>
+    {
+        public int Weight { get; set; }
+        public int Value { get; set; }
+        public Item(int value, int weight)
+        {
+            Value = value;
+            Weight = weight;
+        }
+
+        public int CompareTo(Item other)
+        {
+            if (this.Value < other.Value) return -1;
+            if (this.Value > other.Value) return 1;
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value}:{Weight}";
+        }
+    }
+}
---
+++ 13 Recap/IMS/Knapsack.cs
@@ -0,0 +1,25 @@
+﻿namespace IMS
+{
+    internal class Knapsack
+    {
+        public List<Item> Items { get; }
+        public int MaxWeight { get; set; }
+        public Knapsack(List<Item> items, int max)
+        {
+            Items = items;
+            MaxWeight = max;
+        }
+
+      
+
+        public override string ToString()
+        {
+            string s = "";
+            foreach (var item in Items)
+            {
+                s += item + "\n";
+            }
+            return s;
+        }
+    }
+}
