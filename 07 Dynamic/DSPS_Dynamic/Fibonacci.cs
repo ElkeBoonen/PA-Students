6542345dabb99891e2cf4a7ee09914dea9955b67
EB <elke.boonen@thomasmore.be>
2024-11-08 10:37:49 +0100

    8/11

---
+++ 07 Dynamic/DSPS_Dynamic/Fibonacci.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSPS_Dynamic
+{
+    internal class Fibonacci
+    {
+        public int Recursion(int n)
+        {
+            Console.WriteLine("call with " + n);
+
+            if (n <= 0) return 0;
+            if (n == 1) return 1;
+
+            return Recursion(n - 1) + Recursion(n - 2);
+        
+        }
+
+        public int Iterative(int number)
+        {
+            if (number <= 1) return number;
+
+            int fib1 = 0, fib2 = 1, fib3 = 1;
+
+            for (int i = 2; i < number; ++i)
+            {
+                fib1 = fib2;
+                fib2 = fib3;
+                fib3 = fib1 + fib2;
+            }
+            return fib3;
+        }
+
+        public int Memoization(int n, int[] array)
+        {
+            Console.WriteLine("call with " + n);
+
+            if (n <= 0) return 0;
+            if (n == 1) return 1;
+            if (array[n] > 0) return array[n];
+
+            array[n] = Memoization(n - 1, array) + Memoization(n - 2, array);
+            return array[n];
+        }
+
+        public int Tabulation(int n)
+        {
+            int[] array = new int[n + 1];
+
+            array[0] = 0;
+            array[1] = 1;
+            for (int i = 2; i <= n; ++i)
+            {
+                array[i] = array[i - 1] + array[i - 2];
+            }
+            return array[n];
+        }
+
+    }
+}
---
+++ 07 Dynamic/DSPS_Dynamic/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DSPS_Dynamic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = 10;
+
+            Fibonacci fib = new Fibonacci();
+            //Console.WriteLine(fib.Recursion(5));
+            Console.WriteLine(fib.Iterative(n));
+            Console.WriteLine(fib.Recursion(n));
+            Console.WriteLine(fib.Memoization(n, new int[n+1]));
+            Console.WriteLine(fib.Tabulation(n));
+            //Console.WriteLine(fib.Recursion(1));
+
+        }
+    }
+}
