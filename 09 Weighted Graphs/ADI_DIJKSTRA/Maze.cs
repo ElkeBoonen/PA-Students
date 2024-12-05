a45d0e48e3cf6652b2c8c8516e93ef52fbe08859
EB <elke.boonen@thomasmore.be>
2024-12-05 22:45:22 +0100

    5/12

---
+++ 09 Weighted Graphs/ADI_DIJKSTRA/Maze.cs
@@ -0,0 +1,106 @@
+﻿
+using System.Net;
+
+namespace ADI_DIJKSTRA
+{
+    internal class Maze
+    {
+        List<int>[] maze;
+
+        public Maze(int nodes)
+        {
+            maze = new List<int>[nodes];
+            for (int i = 0; i < nodes; i++)
+            {
+                maze[i] = new List<int>();
+            }
+        }
+
+        internal void AddEdge(int node1, int node2)
+        {
+            maze[node1].Add(node2);
+            maze[node2].Add(node1);
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            for (int i = 0; i < maze.Length; i++)
+            {
+                s += i + " --> ";
+                foreach (int n in maze[i])
+                {
+                    s += n + " ";
+                }
+                s += "\n";
+            }
+            return s;
+        }
+
+        private int FindNextNode(int[] distances, List<int> visited)
+        {
+            int minimum = Int32.MaxValue;
+            int node = -1;
+
+            for (int i = 0; i < distances.Length; i++)
+            {
+                if (distances[i] < minimum && !visited.Contains(i))
+                { 
+                    minimum = distances[i];
+                    node = i;
+                }
+            }
+            return node;
+        }
+
+        internal string Dijkstra(int startnode, int endnode)
+        {
+            List<int> visited = new List<int>();
+
+            int[] distances = new int[maze.Length];
+            int[] previous = new int[maze.Length];
+            for (int i = 0; i < distances.Length; i++)
+            {
+                distances[i] = Int32.MaxValue;
+                previous[i] = -1;
+            }
+
+            distances[startnode] = 0;
+
+            while (true) 
+            { 
+                int node = FindNextNode(distances, visited);
+                if (node == -1) break;
+
+                visited.Add(node);
+
+                foreach (int neighbour in maze[node])
+                {
+                    int cost = Math.Abs(neighbour - node);
+                    int temp_distance = distances[node] + cost;
+
+                    if (!visited.Contains(neighbour) && temp_distance < distances[neighbour])
+                    {
+                        distances[neighbour] = temp_distance;
+                        previous[neighbour] = node;
+                    }
+                }
+            }
+
+            /*for (int i = 0; i < distances.Length; i++)
+            { 
+                Console.WriteLine(i + " --> " + distances[i] + " --> previous node " + previous[i]);
+            }*/
+
+            int target = endnode;
+            List<int> path = new List<int>() { target };
+            while (target != startnode)
+            {
+                target = previous[target];
+                path.Add(target);
+            }
+            path.Reverse();
+            return "path: " + String.Join(" ",path); // 12 17 18 19 14 9 ...
+        }
+    }
+}
---
+++ 09 Weighted Graphs/DSPS_Dijkstra/Maze.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSPS_Dijkstra
+{
+    internal class Maze
+    {
+        List<int>[] maze;
+        public Maze(int nodes)
+        {
+            maze = new List<int>[nodes];
+            for (int i = 0; i < nodes; i++)
+            {
+                maze[i] = new List<int>();
+            }
+        }
+
+        internal void AddEdge(int node1, int node2)
+        {
+            maze[node1].Add(node2);
+            maze[node2].Add(node1);
+        }
+        public override string ToString()
+        {
+            string s = "";
+            for (int i = 0; i < maze.Length; i++)
+            {
+                s += i + " --> ";
+                foreach (int n in maze[i])
+                {
+                    s += n + " ";
+                }
+                s += "\n";
+            }
+            return s;
+        }
+
+
+        private int FindNextNode(int[] distances, List<int> visited)
+        { 
+            int smallest = Int32.MaxValue;
+            int node = -1;
+
+            for (int i = 0; i < distances.Length; i++)
+            {
+                if (!visited.Contains(i) && distances[i] < smallest)
+                { 
+                    smallest = distances[i];
+                    node = i;
+                }
+            }
+            return node;
+        }
+
+        public string Dijkstra(int startnode, int endnode)
+        {
+            int[] distances = new int[maze.Length];
+            int[] previous = new int[maze.Length];
+
+            List<int> visited = new List<int> ();
+            for (int i = 0; i < maze.Length; i++)
+            {
+                distances[i] = Int32.MaxValue;
+                previous[i] = -1;
+            }
+
+            distances[startnode] = 0;
+
+            while (true)
+            { 
+                int nextnode = FindNextNode(distances, visited);
+                if (nextnode == -1) break;
+                visited.Add(nextnode);
+
+                //Console.WriteLine(nextnode);
+
+                foreach (int child in maze[nextnode])
+                {
+                    int temp = Math.Abs(child - nextnode) + distances[nextnode];
+                    if (!visited.Contains(child) && temp < distances[child])
+                    {
+                        distances[child] = temp;
+                        previous[child] = nextnode;
+                    }
+                }
+            
+            }
+
+            int node = endnode;
+            string path = "";
+            while (node != startnode)
+            { 
+                path += node.ToString() + " ";
+                node = previous[node];
+            }
+            path += node;
+            return String.Join("",path.Reverse()) ;
+        
+        }
+    }
+}
