c37679d19543076e1e308fc60dfdc2228d16e6fc
EB <elke.boonen@thomasmore.be>
2025-09-15 11:16:01 +0200

    15/09

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@ namespace PAStudents
         static void Main(string[] args)
         {
             Searching search = new Searching();
+
+            Console.WriteLine($"LINEAR: {search.Linear(5)}");
+            Console.WriteLine($"STUPID: {search.Stupid(5)}");
             Console.ReadLine();
         }
     }
--- Searching.cs
+++ Searching.cs
@@ -11,14 +11,39 @@ namespace PAStudents
             array = new int[] { -10, -7, -5, -2, 0, 1, 2, 4, 5, 7, 9 };
         }
 
-        public int Linear()
-        { }
+        public int Linear(int number)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == number)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
 
-        public int Stupid()
-        { }
+        public int Stupid(int number)
+        {
+            Random rd = new Random(); //https://learn.microsoft.com/en-us/dotnet/api/system.random?view=net-9.0
+            int count = 0;
+            int i = -1;
+            while (count < array.Length)
+            {
+                i = rd.Next(0, array.Length);
+                if (array[i] == number)
+                {
+                    return i;
+                }
+                count++;
+            }
+            return -1;
+        }
 
         public int Binary()
-        { }
+        {
+            return -1;
+        }
     } 
 
 }
