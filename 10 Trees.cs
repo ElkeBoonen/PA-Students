fbceafa089414127f96cb5ff556209f7b85b0181
EB <elke.boonen@thomasmore.be>
2025-11-24 12:34:26 +0100

    24/11 met trees

---
+++ 10 Trees.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PAStudents
+{
+    class TreeNode
+    {
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+
+    }
+
+    class BinaryTree
+    {
+        
+    }
+
+}
